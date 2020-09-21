75ab8c86462ec45e4c7c2a17d185a69956a82975
Justice Payne <spikepayne@yahoo.com>
2020-09-21 23:13:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,20 +9,14 @@ class CashRegister
     @items = []
   end
 
-  def add_item(item, price, quantity = 1)
-    @price = price
+  def add_item(items, price, quantity=1)
     @total += price * quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
+    quantity.times do 
+      @items << items  
     end
+    @last_trans = price * quantity
   end
-
+  
   def apply_discount
     if @discount > 0
       @to_take_off = (price * discount)/100
